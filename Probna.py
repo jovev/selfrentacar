238cb751dc5e45ac9e9d79d5f4e86fe90cccf59d
Radmila <radmila.151198@gmail.com>
2023-01-25 12:34:53 +0100

    Probna1

--- Probna.py
+++ Probna.py
@@ -1,3 +1,12 @@
 self = "Rent a car"
 print(self)
+import time
+for i in range(10,0,-1):
+    print(i)
+    time.sleep(1)
+print("Srecna Nova godina")
 
+imena = {"Vucic": "Radmila",
+         "Markovic": "Ilija",
+         "Nikolic": "Marko"}
+print(imena["Markovic"])
