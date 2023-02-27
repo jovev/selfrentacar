16c8ec5566af21c05d5b1af66fe7c19632d0930e
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2023-02-27 14:46:58 +0100

    Dodata funkcija za kreiranje sloga iz emaila

--- fleet_rental/models/car_rental.py
+++ fleet_rental/models/car_rental.py
@@ -552,6 +552,34 @@ class CarRentalContract(models.Model):
         else:
             raise ValidationError("Please enter advance amount to make first payment")
 
+    @api.model
+    def message_new(self, msg, custom_values=None):
+        """ Overrides mail_thread message_new that is called by the mailgateway
+            through message_process.
+            This override updates the document according to the email.
+        """
+        # remove default author when going through the mail gateway. Indeed we
+        # do not want to explicitly set user_id to False; however we do not
+        # want the gateway user to be responsible if no other responsible is
+        # found.
+        create_context = dict(self.env.context or {})
+        create_context['default_user_ids'] = False
+        if custom_values is None:
+            custom_values = {}
+        defaults = {
+            'name': msg.get('subject') or _("No Subject"),
+            'manager_id': "WEB BOOKING",
+            'customer_id': msg.get('author_id'),
+        }
+        defaults.update(custom_values)
+
+        rent = super(Rental, self.with_context(create_context)).message_new(msg, custom_values=defaults)
+        email_list = rent.email_split(msg)
+        partner_ids = [p.id for p in self.env['mail.thread']._mail_find_partner_from_emails(email_list, records=rent,
+                                                                                            force_create=False) if p]
+        rent.message_subscribe(partner_ids)
+        return rent
+
 
 class FleetRentalLine(models.Model):
     _name = 'fleet.rental.line'
