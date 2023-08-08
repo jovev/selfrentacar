2fdbc471204508bde900273aa4128ed87f494f2d
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2023-08-09 00:00:15 +0200

    Dodat modul za prikaz ugovora preko portala

--- ii_carrental_portal/controllers/portal.py
+++ ii_carrental_portal/controllers/portal.py
@@ -415,5 +415,23 @@ class PaymentPortal(payment_portal.PaymentPortal):
         )
 
 
-    def get_portal_url(self):
-        return ''
+    def get_portal_url(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
+        """
+            Get a portal url for this model, including access_token.
+            The associated route must handle the flags for them to have any effect.
+            - suffix: string to append to the url, before the query string
+            - report_type: report_type query string, often one of: html, pdf, text
+            - download: set the download query string to true
+            - query_string: additional query string
+            - anchor: string to append after the anchor #
+        """
+        self.ensure_one()
+        url = self.access_url + '%s?access_token=%s%s%s%s%s' % (
+            suffix if suffix else '',
+            self._portal_ensure_token(),
+            '&report_type=%s' % report_type if report_type else '',
+            '&download=true' if download else '',
+            query_string if query_string else '',
+            '#%s' % anchor if anchor else ''
+        )
+        return url
