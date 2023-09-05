7559c1b20366e523fabf97cd8e218cb275c75b01
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2023-09-06 00:18:18 +0200

    Dodat modul za prikaz ugovora preko portala

--- fleet_operations/__manifest__.py
+++ fleet_operations/__manifest__.py
@@ -13,7 +13,7 @@
     "author": "Serpent Consulting Services Pvt. Ltd.",
     "website": "https://www.serpentcs.com",
     # Dependencies
-    "depends": ["fleet", "stock", "account"],
+    "depends": ["fleet", "stock", "account", "fleet_rental"],
     # Data
     "data": [
         "security/fleet_security.xml",
