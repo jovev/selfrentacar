fbb2f4d43f28fcfebbe54770749862a1089ea392
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2023-09-13 21:51:53 +0000

    Update __manifest__.py

--- rentomat_configurator/__manifest__.py
+++ rentomat_configurator/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "IRVAS Int. doo",
+    'website': "https://www.irvas.rs",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
