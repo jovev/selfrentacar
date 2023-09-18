0b5c40a65dfd7cd14dd8cfa1ef18c819b70609fe
petar <petar.jovanovic@naisrobotics.com>
2023-09-18 11:22:09 +0100

    nesto

--- rentomat_configurator/__manifest__.py
+++ rentomat_configurator/__manifest__.py
@@ -23,11 +23,11 @@
     'depends': ['base', 'stock'],
 
     # always loaded
-    # 'data': [
-    #     # 'security/ir.model.access.csv',
-    #     'views/views.xml',
-    #     'views/templates.xml',
-    #],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        # 'views/templates.xml',
+    ],
     # only loaded in demonstration mode
     # 'demo': [
     #     'demo/demo.xml',
