e7f0b0dc2f37b4e878ba5c5127db45ddebc36c2e
Petar Jovanovic <petar.jovanovic@naisrobotics.com>
2023-09-18 09:53:06 +0100

    Definisan osnovni model podataka za rentomate

--- rentomat_configurator/models/models.py
+++ rentomat_configurator/models/models.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class rentomat_configurator(models.Model):
-#     _name = 'rentomat_configurator.rentomat_configurator'
-#     _description = 'rentomat_configurator.rentomat_configurator'
+class rentomat_configurator(models.Model):
+    _name = 'rentomat.configurator'
+    _description = 'Rentomat configurator containing key RFID positions'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    rentomat_id = fields.Char(string = "Rentomat ID")
+    location_id = fields.Meny2one("stock.location", string = "Rentomat location")
+    max_rentomat_capacity = fields.Int("Rentomat capacity")
+    position_1 = fields.Char("RFID for position 1")
+    position_2 = fields.Char("RFID for position 2")
+    position_3 = fields.Char("RFID for position 3")
+    position_4 = fields.Char("RFID for position 4")
+    position_5 = fields.Char("RFID for position 5")
+    position_6 = fields.Char("RFID for position 6")
+    position_7 = fields.Char("RFID for position 7")
+    position_8 = fields.Char("RFID for position 8")
+    position_9 = fields.Char("RFID for position 9")
+    position_10 = fields.Char("RFID for position 10")
+    position_11 = fields.Char("RFID for position 11")
+    position_12 = fields.Char("RFID for position 12")
+    
+
+
+
+    description = fields.Text("Description")
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     for record in self:
+    #         record.value2 = float(record.value) / 100
