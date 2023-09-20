1b7fc31979dde2c34d0ba609bb4d0dbd5c1f5daa
Dragan <dragan.mitic@irvas.rs>
2023-09-20 13:04:11 +0000

    Update __manifest__.py

--- ii_carrental_portal/__manifest__.py
+++ ii_carrental_portal/__manifest__.py
@@ -52,7 +52,7 @@ This module contains all the common features of Sales Management and eCommerce.
      #   'views/sale_order_line_views.xml',
         'views/sale_portal_templates.xml',
     #    'views/utm_campaign_views.xml',
-	     'static/src/xml/name_and_signature.xml',
+
     #    'views/sale_menus.xml',  # Last because referencing actions defined in previous files
     ],
     'demo': [
@@ -75,6 +75,7 @@ This module contains all the common features of Sales Management and eCommerce.
             'sale/static/src/js/sale_portal_sidebar.js',
             'sale/static/src/js/sale_portal.js',
             'sale/static/src/js/payment_form.js',
+            'static/src/xml/name_and_signature.xml',
         ],
         'web.assets_tests': [
             'sale/static/tests/tours/**/*',
