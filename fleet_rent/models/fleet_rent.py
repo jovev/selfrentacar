cf12ce7be8cec0e734472dfb1a785d31362f6332
Dragan <dragan.mitic@irvas.rs>
2023-09-25 10:07:07 +0000

    Update fleet_rent.py

--- fleet_rent/models/fleet_rent.py
+++ fleet_rent/models/fleet_rent.py
@@ -639,6 +639,38 @@ class FleetRent(models.Model):
                 seq = self.env["ir.sequence"].next_by_code("fleet.rent")
                 rent_vals.update({"name": seq})
             rent.write(rent_vals)
+        self.action_rentconfirmation_send()
+            
+    # ovo je preuzeto iz Sales Order
+    def action_rentconfirmation_send(self):
+        """ Opens a wizard to compose an email, with relevant mail template loaded by default """
+        self.ensure_one()
+        self.order_line._validate_analytic_distribution()
+        lang = self.env.context.get('lang')
+        mail_template = self._find_mail_template()
+        if mail_template and mail_template.lang:
+            lang = mail_template._render_lang(self.ids)[self.id]
+        ctx = {
+            'default_model': 'fleet.rent',
+            'default_res_id': self.id,
+            'default_use_template': bool(mail_template),
+            'default_template_id': mail_template.id if mail_template else None,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True,
+            'model_description': self.with_context(lang=lang).type_name,
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(False, 'form')],
+            'view_id': False,
+            'target': 'new',
+            'context': ctx,
+        }
 
     def action_rent_close(self):
         """Method to Change rent state to close."""
