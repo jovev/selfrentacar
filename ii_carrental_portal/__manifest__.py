8a1cb5a538cff9a5ba7d92c7368275facbb699d1
Dragan <dragan.mitic@irvas.rs>
2023-10-09 13:12:08 +0000

    Update __manifest__.py

--- ii_carrental_portal/__manifest__.py
+++ ii_carrental_portal/__manifest__.py
@@ -21,8 +21,8 @@ This module contains all the common features of Sales Management and eCommerce.
 
     #    'report/account_invoice_report_views.xml',
         'report/ir_actions_report_templates.xml',
-        'report/fleet_rent.xml',
-        'report/key_return_report.xml',
+    #    'report/fleet_rent.xml',
+    #    'report/key_return_report.xml',
     #   'report/ir_actions_report.xml',
     #    'report/sale_report_views.xml',
 
