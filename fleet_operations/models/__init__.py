b6b9d7be9a4c9862becff0cd9d689758b710011a
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2024-02-13 15:37:33 +0100

    Prosirenje modela res.partners prebaceno u modul fleet_rental

--- fleet_operations/models/__init__.py
+++ fleet_operations/models/__init__.py
@@ -3,5 +3,5 @@
 from . import fleet_extended
 from . import fleet_service
 from . import update_pending_history
-from . import res_users
+# from . import res_users
 from . import fleet_operation_account
