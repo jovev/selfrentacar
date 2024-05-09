a664f77aa90924545df09b13388f6ffcf9d64a2b
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2024-05-09 16:20:50 +0200

    Promena u kreiranju novog ugovora preko web rezervacije

--- fleet_rental/models/car_rental.py
+++ fleet_rental/models/car_rental.py
@@ -119,12 +119,12 @@ def pars_html_table(data):
                 kolona2_gp = columns[1].text.strip()
                 my_dic['Grand Price'] = kolona2_gp
 
-            if last_col_name == "Deposit:" or last_col_name == "Depozit:":
-                kolona1 = columns[0].text.strip()
-                kolona2_d = columns[1].text.strip()
-                my_dic['Deposit'] = kolona2_d
-            else:
-                my_dic['Deposit'] = "0.0"
+            # if last_col_name == "Deposit:" or last_col_name == "Depozit:":
+            #     kolona1 = columns[0].text.strip()
+            #     kolona2_d = columns[1].text.strip()
+            #     my_dic['Deposit'] = kolona2_d
+            # else:
+            #     my_dic['Deposit'] = "0.0"
 
             if last_col_name == "Pick-up Date & Time" or last_col_name == "Datum i vrijeme preuzimanja":
                 kolona1 = columns[0].text.strip()
