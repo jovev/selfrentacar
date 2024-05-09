634abead48f8251ac79cd2abaa9aa1fb9522fca1
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2024-05-09 23:55:18 +0200

    Izmene vezane za pruzimanja grand_price

--- fleet_rental/models/car_rental.py
+++ fleet_rental/models/car_rental.py
@@ -125,10 +125,10 @@ def pars_html_table(data):
                 my_dic['Grand Price'] = kolona2_gp
                 continue
 
-            if kolona1 == "Deposit:" or last_col_name == "Depozit:":
-                kolona1 = columns[0].text.strip()
-                kolona2_d = columns[1].text.strip()
-                my_dic['Deposit'] = kolona2_d
+            # if kolona1 == "Deposit:" or last_col_name == "Depozit:":
+            #     kolona1 = columns[0].text.strip()
+            #     kolona2_d = columns[1].text.strip()
+            #     my_dic['Deposit'] = kolona2_d
             # else:
             #     my_dic['Deposit'] = "0.0"
 
