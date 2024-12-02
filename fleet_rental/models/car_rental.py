7ff5412a51e46f7c7872b22dc6a5fa346076ece7
Ljubisa Jovev <ljubisa.jovev@irvas.rs>
2024-12-02 17:01:21 +0100

    Pars 3. tabele

--- fleet_rental/models/car_rental.py
+++ fleet_rental/models/car_rental.py
@@ -122,7 +122,7 @@ def pars_html_table(data):
                 _logger.info('Lokacija Preuzimanja = %s', kolona1)
                 _logger.info('Lokacija vracanja = %s', kolona2)
                 my_dic['Rent from'] = kolona1
-                my_dic['Return location'] = kolona2
+            #    my_dic['Return location'] = kolona2
                 continue
 
             # ukupan iznos
